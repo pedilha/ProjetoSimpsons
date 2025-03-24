adc555ee8e021d2ffd1c208d3649b52bf1dc9431
pedilha <pedrowilian13@gmail.com>
2025-03-24 20:04:16 -0300

    first commit

---
+++ app/src/main/java/edu/pucgoias/projetosimpsons/MainActivity.java
@@ -0,0 +1,45 @@
+package edu.pucgoias.projetosimpsons;
+
+import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView lista = findViewById(R.id.lista); // criar a lista de itens
+
+        Pessoa lisa = new Pessoa("Lisa Simpson",
+                "200412212", "ok", "lisa_simpson");
+        Pessoa homer = new Pessoa("Homer Simpson",
+                "200412222", "off", "homer_simpson");
+        Pessoa marge = new Pessoa("Marge Simpson",
+                "200412221", "off", "marge_simpson");
+        Pessoa bart = new Pessoa("Bart Simpson",
+                "200412211", "ok", "bart_simpson");
+
+        ArrayList<Pessoa> pessoas = new ArrayList<>();
+        pessoas.add(lisa);
+        pessoas.add(homer);
+        pessoas.add(marge);
+        pessoas.add(bart);
+
+        ListaAdapter adapter = new ListaAdapter(this, R.layout.layout_lista, pessoas);
+        lista.setAdapter(adapter);
+
+    }
+}
